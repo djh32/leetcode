258279af90b12d9712732e5a727edb9d8ee94635
johnnydu <johnnydu@tencent.com>
2024-11-25 13:10:34 +0800

    update

--- myself_leet/test.py
+++ myself_leet/test.py
@@ -1,14 +1,51 @@
-import numpy as np  
-import matplotlib.pyplot as plt 
-from scipy import stats
+#coding = utf-8
 
-X = np.linspace(0, 1600, 10)
 
-std = 0.693147182
-mean = 1
-lognorm_distribution = stats.lognorm([std], loc=mean)
-lognorm_distribution_pdf = lognorm_distribution.pdf(X)
-fig, ax = plt.subplots(figsize=(8, 5))
-plt.plot(X, lognorm_distribution_pdf, label="μ=0, σ=1")
-ax.set_xticks(X)
-plt.show()
+
+def bin_search(nums,val):
+    l,r = 0, len(nums)
+    while l < r:
+        mid_idx = (l+r)//2
+        if val > nums[mid_idx]:
+            l = mid_idx+1
+        elif val < nums[mid_idx]:
+            r = mid_idx
+        else:
+            return mid_idx
+    return r-1 # r-1是精髓，能找到比val小的。 [4] val = 4-9都是0的idx ，0-3是-1的idx
+
+def find_small_nums(given,real):
+    real = [int(x) for x in real]
+    mark_need_max = False
+    result = []
+    for idx in range(len(real)):
+        find_idx = bin_search(given,real[idx])
+        if find_idx == -1 or mark_need_max == True:
+            #直接用最大的padding少一位
+            res = [given[-1] for _ in range(len(real) - idx -1)]
+            result.extend(res)
+            return result
+        else:
+            result.append(given[find_idx])
+
+        if given[find_idx] < real[find_idx]: # 如果第一次出现不能匹配的数据以后，后面的直接用最大的拼接就行
+            mark_need_max = True
+
+    return result
+
+
+print(find_small_nums([2,w9],"529"))
+#print(bin_search([4],3))
+#print(bin_search([5,5,5,6],6))
+
+
+
+
+
+
+
+
+
+
+
+
