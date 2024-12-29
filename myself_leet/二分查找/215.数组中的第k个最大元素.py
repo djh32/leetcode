5d4eafba712fb299f52e0f33cfced02311242469
johnnydu <johnnydu@tencent.com>
2024-12-29 23:19:03 +0800

    update

--- myself_leet/二分查找/215.数组中的第k个最大元素.py
+++ myself_leet/二分查找/215.数组中的第k个最大元素.py
@@ -9,19 +9,9 @@ import random
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        def quick_sort(nums, lidx, ridx):
-            pivot = random.randint(lidx, ridx)
-            cache = nums[pivot]
-            while lidx < ridx:
-                while lidx < ridx and nums[lidx] <= nums[pivot]:
-                    lidx += 1
-                nums[lidx] = nums[ridx]
-                while lidx < ridx and nums[ridx] >= nums[pivot]:
-                    ridx -= 1
-                nums[ridx] = nums[lidx]
-
-    def findKthLargest2(self, nums: List[int], k: int) -> int:
-        def quick_sort(nums, lidx, ridx):
+        def quick_sort(nums, lidx, ridx):  # quick sort 按照左pivot
+            rand_idx = random.randint(lidx, ridx)
+            nums[lidx], nums[rand_idx] = nums[rand_idx], nums[lidx]
             pivot = lidx
             cache = nums[pivot]
             while lidx < ridx:
@@ -34,21 +24,37 @@ class Solution:
             nums[lidx] = cache
             return lidx, nums[lidx]
 
-        # pivot_idx = random.randint(0, len(nums) - 1)  # 随机选择pivot
-        # nums[0], nums[pivot_idx] = nums[pivot_idx], nums[0]  # pivot放置到最左边
-        random.shuffle(nums)
+        def quick_sort_2(nums, lidx, ridx):  # quick sort 按照左 第一个index 做pivot 都正确
+            pivot = lidx
+            while lidx < ridx:
+                while lidx < ridx and nums[ridx] >= nums[pivot]:
+                    ridx -= 1
+                while lidx < ridx and nums[lidx] <= nums[pivot]:
+                    lidx += 1
+                nums[lidx], nums[ridx] = nums[ridx], nums[lidx]
+            nums[lidx], nums[pivot] = nums[pivot], nums[lidx]
+            return lidx, nums[lidx]
+
+        # random.shuffle(nums)
         find_now, _ = quick_sort(nums, 0, len(nums) - 1)
         l = len(nums)
+        left, right = 0, l - 1
         need = l - k
         while find_now != need:
             if find_now < need:
-                find_now, _ = quick_sort(nums, find_now + 1, len(nums) - 1)
+                while find_now < need and find_now + 1 < need and nums[find_now + 1] == nums[find_now]:
+                    find_now += 1
+                left = find_now + 1
+                find_now, _ = quick_sort(nums, left, right)
             else:
-                find_now, _ = quick_sort(nums, 0, find_now - 1)
+                while find_now > need and find_now - 1 > need and nums[find_now - 1] == nums[find_now]:
+                    find_now -= 1
+                right = find_now - 1
+                find_now, _ = quick_sort(nums, left, right)
         return nums[find_now]
 
 
-def quick_sort(nums, lidx, ridx):
+def quick_sort(nums, lidx, ridx):  # quick sort https://blog.csdn.net/qq_69369227/article/details/130040525
     pivot = 0
     cache = nums[pivot]
     while lidx < ridx:
@@ -62,7 +68,7 @@ def quick_sort(nums, lidx, ridx):
     return nums
 
 
-nums = [3,2,1,5,6,4]
+nums = [3, 2, 1, 4, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 # print(quick_sort(nums, 0, len(nums) - 1))
 
-print(Solution().findKthLargest2(nums, 2))
+print(Solution().findKthLargest2(nums, 5))
