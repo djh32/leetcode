fe3ac8aa3b9c445a6103b4ed659fb0c44114b353
johnnydu <johnnydu@tencent.com>
2025-01-03 12:04:52 +0800

    update

---
+++ myself_leet/堆训练/heapify练习.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+from typing import List
+import heapq
+# todo 实现heapify
+
+
+
--- myself_leet/面试遇到/同事-字节-数据流中找中位数.py
+++ myself_leet/面试遇到/同事-字节-数据流中找中位数.py
@@ -1,47 +1,88 @@
 # coding=utf-8
-'''
-删除多余空格
+from typing import List
+import heapq
+"""
+中位数是有序整数列表中的中间值。如果列表的大小是偶数，则没有中间值，中位数是两个中间值的平均值。
 
-题目描述
-实现一个函数，要求
-输入："                   I         am         a         bytedancer.       "
-输出："I am a bytedancer."
-'''
-# todo 待更新， 堆栈训练里面有原题
+例如 arr = [2,3,4] 的中位数是 3 。
+例如 arr = [2,3] 的中位数是 (2 + 3) / 2 = 2.5 。
+实现 MedianFinder 类:
 
-def remove_extra_spaces(s: str) -> str:  # gpt
-    result = []
-    in_space = False  # 标记当前是否在空格中
+MedianFinder() 初始化 MedianFinder 对象。
 
-    for char in s:
-        if char != ' ':
-            result.append(char)  # 如果不是空格，直接添加
-            in_space = False  # 进入非空格状态
-        elif not in_space:  # 如果是空格且不在空格状态
-            result.append(' ')  # 添加一个空格
-            in_space = True  # 进入空格状态
+void addNum(int num) 将数据流中的整数 num 添加到数据结构中。
 
-    # 去掉开头和结尾的空格
-    return ''.join(result).strip()
+double findMedian() 返回到目前为止所有元素的中位数。与实际答案相差 10-5 以内的答案将被接受。
 
+示例 1：
 
-def remove_extra_spaces_without_strip(s: str) -> str:  # 自己写
-    in_space_area = False
-    res = []
-    for c in s:
-        if c != " ":
-            res.append(c)
-            in_space_area = False
-        else:  # c == " "
-            if in_space_area == False:
-                res.append(" ")
-            in_space_area = True
-    if res[0] == " ": res = res[1:]
-    if res[-1] == " ": res = res[:-1]
-    return "".join(res)
+输入
+["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
+[[], [1], [2], [], [3], []]
+输出
+[null, null, null, 1.5, null, 2.0]
+
+解释
+MedianFinder medianFinder = new MedianFinder();
+medianFinder.addNum(1);    // arr = [1]
+medianFinder.addNum(2);    // arr = [1, 2]
+medianFinder.findMedian(); // 返回 1.5 ((1 + 2) / 2)
+medianFinder.addNum(3);    // arr[1, 2, 3]
+medianFinder.findMedian(); // return 2.0
 
 
-# 示例
-input_str = "      Hello      World!"
-output_str = remove_extra_spaces_without_strip(input_str)
-print(output_str)  # 输出: "Hello World!"
+"""
+
+import heapq
+
+
+class MedianFinder:
+
+    def __init__(self):
+        self.bg_first_hp = []
+        self.sm_first_hp =[]
+
+    def addNum(self, num: int) -> None:
+        if len(self.bg_first_hp) == len(self.sm_first_hp):
+            heapq.heappush(self.bg_first_hp,-num)
+            min = heapq.heappop(self.bg_first_hp)
+            heapq.heappush(self.sm_first_hp,-min)
+        else:
+            heapq.heappush(self.sm_first_hp,num)
+            max_hp = heapq.heappop(self.sm_first_hp)
+            heapq.heappush(self.bg_first_hp,-max_hp)
+
+    def findMedian(self) -> float:
+        if len(self.bg_first_hp) == len(self.sm_first_hp):
+            heapq.heapify(self.sm_first_hp)
+            heapq.heapify(self.bg_first_hp)
+            res = (self.sm_first_hp[0] -self.bg_first_hp[0])/2.0
+        else:
+            heapq.heapify(self.sm_first_hp)
+            res = self.sm_first_hp[0]
+        return res
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
+
+
+
+mf = MedianFinder()
+mf.addNum(1)
+mf.addNum(3)
+mf.addNum(5)
+mf.addNum(7)
+mf.addNum(9)
+mf.addNum(11)
+mf.addNum(13)
+mf.addNum(15)
+mf.findMedian()
+mf.addNum(17)
+mf.addNum(19)
+
+pass
+
+
+
